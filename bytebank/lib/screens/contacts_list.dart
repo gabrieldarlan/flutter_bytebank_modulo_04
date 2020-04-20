541baedf93af928c4a7f7f7b802fdd36fc10660d
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-04-19 22:26:17 -0300

    cap 2completo

---
+++ bytebank/lib/screens/contacts_list.dart
@@ -0,0 +1,50 @@
+import 'package:bytebank/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactList extends StatefulWidget {
+  const ContactList({Key key}) : super(key: key);
+
+  @override
+  _ContactListState createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Contacts'),
+      ),
+      body: ListView(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+              title: Text(
+                'Siamesa',
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+              ),
+              subtitle: Text(
+                '1000',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context)
+              .push(MaterialPageRoute(
+                builder: (context) => ContactForm(),
+              ))
+              .then((newContact) => debugPrint(newContact.toString()));
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
